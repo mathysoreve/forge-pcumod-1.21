49da96c54bf27f71c2f15d9590a6c50958cd265b
oreve <orevemathys4@gmail.com>
2024-08-06 11:54:17 +0200

    update

--- src/main/java/net/awaren/pcumod/PCUMod.java
+++ src/main/java/net/awaren/pcumod/PCUMod.java
@@ -17,7 +17,7 @@ import org.slf4j.Logger;
 @Mod(PCUMod.MOD_ID)
 public class PCUMod {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "pcu_mod";
+    public static final String MOD_ID = "pcumod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 

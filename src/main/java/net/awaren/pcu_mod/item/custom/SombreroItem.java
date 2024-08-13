33f02b4fa119027c6c7d0335700b7dbb0b5d4a2c
Heyfifi0 <thomaschevalierpro@gmail.com>
2024-08-13 18:32:56 +0200

    sombrero buggé (tjrs pas de modèle)

--- src/main/java/net/awaren/pcu_mod/item/custom/SombreroItem.java
+++ src/main/java/net/awaren/pcu_mod/item/custom/SombreroItem.java
@@ -1,27 +1,43 @@
 package net.awaren.pcu_mod.item.custom;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.extensions.IForgeItem;
+
+import java.util.List;
 
 public class SombreroItem extends ArmorItem implements IForgeItem {
 
-    // TODO : faire en sorte que les squelettes n'attaquent pas
+    // TODO :
+    //  - afficher le modèle 3D SUR LA TETE -E
 
     public SombreroItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
         super(pMaterial, pType, pProperties);
     }
 
+    // Comportement des squelettes -E
 
     @Override
-    public boolean canEquip(ItemStack stack, EquipmentSlot armorType, Entity entity) {
-        if (armorType == EquipmentSlot.HEAD) {
-            return true;
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        if (!level.isClientSide() && player.getItemBySlot(EquipmentSlot.HEAD) == stack) {
+
+            level.getEntitiesOfClass(Skeleton.class, player.getBoundingBox().inflate(15))
+                    .forEach(skeleton -> {
+
+                        skeleton.getNavigation().stop();
+                        skeleton.setAggressive(false);
+                        skeleton.setTarget(null);
+
+                    });
         }
-        return false;
     }
+
 }

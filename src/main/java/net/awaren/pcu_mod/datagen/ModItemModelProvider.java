8da94d6b454fa7473f6d09d49379ca2be1f9cd24
oreve <orevemathys4@gmail.com>
2024-08-19 15:07:51 +0200

    JE COMMIT

--- src/main/java/net/awaren/pcu_mod/datagen/ModItemModelProvider.java
+++ src/main/java/net/awaren/pcu_mod/datagen/ModItemModelProvider.java
@@ -5,6 +5,7 @@ import net.awaren.pcu_mod.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
